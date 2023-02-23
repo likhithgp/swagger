472ce0334e4157a486fb26840c68bb3a98ec122f
likhithgp <52875503+likhithgp@users.noreply.github.com>
2023-02-23 09:46:05 +0530

    Update BookConfig.java

--- main/java/BookConfig.java
+++ main/java/BookConfig.java
@@ -19,3 +19,33 @@ public class BookConfig {
     }
 
 }
+/*
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+//In this example, the api() method creates a new Docket instance and specifies that
+  it should scan the com.example.myapp.controller package for API controllers
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.example.myapp.controller"))
+            .paths(PathSelectors.any())
+            .build()
+            .apiInfo(apiInfo());
+    }
+
+//The apiInfo() method creates an ApiInfo instance that specifies the API
+title, description, version, contact information, and licensing information.
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("My API")
+            .description("API documentation for My App")
+            .version("1.0.0")
+            .contact(new Contact("John Doe", "https://www.example.com", "john.doe@example.com"))
+            .license("Apache 2.0")
+            .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+            .build();
+    }
+}*/
